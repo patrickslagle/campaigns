ea73034c2e857a5bceeed9de626a56124820ad86
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-26 20:54:27 -0800

    setup basic express server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const filePath = path.resolve(__dirname, '../public');
+app.use(express.static(filePath));
+app.listen(3000, () => console.log('Listening on PORT: 3000'));
