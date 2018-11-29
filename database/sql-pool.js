b396cf978271205ac19239af0ecd6ccec058c1a1
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-28 22:15:50 -0800

    changed filename of sql pool connection

--- database/sql-pool.js
+++ database/sql-pool.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  connectionString: 'postgres://nyuzgibb:dgptSbjP7otVsSfbIdD-g3bp06z0VJIo@baasu.db.elephantsql.com:5432/nyuzgibb  ' 
+  connectionString: 'postgres://nyuzgibb:dgptSbjP7otVsSfbIdD-g3bp06z0VJIo@baasu.db.elephantsql.com:5432/nyuzgibb',
 });
 
 module.exports = pool;
