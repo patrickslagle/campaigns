f68598b50d2e6f170f6118d4eb5c967ac10a4ef4
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-28 21:15:26 -0800

    added a database connection file

---
+++ database/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  connectionString: 'postgres://nyuzgibb:dgptSbjP7otVsSfbIdD-g3bp06z0VJIo@baasu.db.elephantsql.com:5432/nyuzgibb  ' 
+});
+
+module.exports = pool;
