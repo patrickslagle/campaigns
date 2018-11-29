c252aff1d30b60166480096cc4f426f4cdede1b9
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-28 21:16:44 -0800

    added functionality for handling errors

--- server/server.js
+++ server/server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const path = require('path');
-const os = require('os');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const app = express();
+app.use(bodyParser.json());
 
+// serve static files
 const filePath = path.resolve(__dirname, '../public');
 app.use(express.static(filePath));
 
-// find user's IP address (IPv4 and non internal address)
-async function findIP(req, res) {
-  const ifaces = os.networkInterfaces();
-
-  Object.keys(ifaces).forEach((ifname) => {
-    ifaces[ifname].forEach((iface) => {
-      // find IPv4 non internal (i.e. 127.0.0.1) ip address
-      if (iface.family === 'IPv4' && !iface.internal) {
-        console.log(iface.address);
-      }
-    });
-  });
-}
-
-app.get('/test', findIP);
+// route different requests
+router(app);
+app.use((err, req, res, next) => {
+  res.json({ error: err.message });
+});
 
 app.listen(3000, () => console.log('Listening on PORT: 3000'));
