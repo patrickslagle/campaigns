f438ef0856460ddbe1cb4030c32f671a904b0504
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-30 08:18:45 -0800

    removed my testing database connectrion string

--- database/sql-pool.js
+++ database/sql-pool.js
@@ -4,7 +4,11 @@ const pool = new Pool({
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  connectionString: 'postgres://nyuzgibb:dgptSbjP7otVsSfbIdD-g3bp06z0VJIo@baasu.db.elephantsql.com:5432/nyuzgibb',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
