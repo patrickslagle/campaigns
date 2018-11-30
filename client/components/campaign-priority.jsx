01998bc4a9d9586b29fa26916ae271e0b0dedbc0
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-29 22:50:36 -0800

    added functionality to display priorities

--- client/components/campaign-priority.jsx
+++ client/components/campaign-priority.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CampaignPriority = ({ priority }) => {
+  function renderPriorities() {
+    return priority.map((item, i) => (
+      <div className="priority">
+        <p>{item}</p>
+        {i !== priority.length - 1 && <button id="arrow-down" type="button" />}
+        {i !== 0 && <button id="arrow-up" type="button" />}
+      </div>
+    ));
+  }
 
   return (
-    <div>
-      {priority}
+    <div id="priority-container">
+      {renderPriorities()}
     </div>
   );
 };
